4a540941b0aad10c97589ab37b8dd8035e0d1eb5
ravindra579 <64942306+ravindra579@users.noreply.github.com>
2020-10-25 19:42:46 +0530

    Add files via upload

---
+++ fom_header_files.cpp
@@ -0,0 +1,23 @@
+#include"iostream"
+#include"Header1.h"
+using namespace sorting_algo;
+using namespace std;
+int main(){
+ int a[5]={8,3,2,6,1};
+ int size_=5;
+ sorting_algos obj1;
+ //------------for sorting array ------------------
+ //obj1.insertion_sort(a,size_);
+ //obj1.selection_sort(a,size_);
+ //obj1.bubble_sort(a,size_);
+ //obj1.merge_sort(a,0,4);
+ //obj1.counting_sort(a,size_);
+ //obj1.shell_sort(a,size_);
+ //obj1.radix_sort(a,size_);
+ //-------------------------------------------------
+ search_algos obj2;
+ //------------- to search a number in array------------
+ //obj2.binarysearch(a,n,size_);//where n is the number to search
+ //obj2.linear_search(a,size_);
+ //---------------------------------------------------
+}
